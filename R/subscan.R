e9253a3ffb0bad5f4c25f65e31735618f03c872e
Roger J. Bos <roger.bos@gmail.com>
2021-11-17 15:55:59 -0500

    test

---
+++ R/subscan.R
@@ -0,0 +1,244 @@
+# Contact api@subscan.io to ket api key
+library(httr)
+library(jsonlite)
+library(data.table)
+
+# MUST SET `subscan_api_key` with your private api key
+# Contact api@subscan.io to ket api key
+api_header <- 'x-api-key=' %+% subscan_api_key
+
+#' Helper function wrapper to paste0()
+#' @name `%+%`
+#'
+#' @author Roger J. Bos, \email{roger.bos@@gmail.com}
+#' @export
+`%+%` <- function(a, b) paste0(a, b)
+
+endpoint_list <- c("Polkadot","polkadot.api.subscan.io",
+                   "Kusama","kusama.api.subscan.io",
+                   "Darwinia","darwinia.api.subscan.io",
+                   "Crust","crust.api.subscan.io",
+                   "Acala Mandala","acala-testnet.api.subscan.io",
+                   "Astar","astar.api.subscan.io",
+                   "Bifrost Asgard","bifrost.api.subscan.io",
+                   "Centrifuge","centrifuge.api.subscan.io",
+                   "ChainX","chainx.api.subscan.io",
+                   "Clover","clover.api.subscan.io",
+                   "Clover Testnet","clover-testnet.api.subscan.io",
+                   "Crust Maxwell","maxwell.api.subscan.io",
+                   "Darwinia Crab","crab.api.subscan.io",
+                   "DataHighway","datahighway.api.subscan.io",
+                   "DataHighway","Harbour	datahighway-harbour.api.subscan.io",
+                   "DeepBrain Chain","dbc.api.subscan.io",
+                   "Dock","dock.api.subscan.io",
+                   "Edgeware","edgeware.api.subscan.io",
+                   "Equilibrium","equilibrium.api.subscan.io",
+                   "Gateway","gateway-testnet.api.subscan.io",
+                   "Karura","karura.api.subscan.io",
+                   "Kulupu","kulupu.api.subscan.io",
+                   "Khala","khala.api.subscan.io",
+                   "KILT Peregrine","kilt-testnet.api.subscan.io",
+                   "Laminar","TC2	laminar-testnet.api.subscan.io",
+                   "Litentry","litentry.api.subscan.io",
+                   "Manta","manta-testnet.api.subscan.io",
+                   "Moonbase","moonbase.api.subscan.io",
+                   "Moonriver","moonriver.api.subscan.io",
+                   "Pangolin","pangolin.api.subscan.io",
+                   "Pangoro","pangoro.api.subscan.io",
+                   "Phala","Rorschach	phala.api.subscan.io",
+                   "Polkadex","polkadex.api.subscan.io",
+                   "Polymesh","polymesh.api.subscan.io",
+                   "Polymesh-test","polymesh-testnet.api.subscan.io",
+                   "Plasm","plasm.api.subscan.io",
+                   "Reef","reef.api.subscan.io",
+                   "Robonomics","robonomics-testnet.api.subscan.io",
+                   "Rococo","rococo.api.subscan.io",
+                   "Sakura","sakura.api.subscan.io",
+                   "Shibuya","shibuya.api.subscan.io",
+                   "Shiden","shiden.api.subscan.io",
+                   "SORA","sora.api.subscan.io",
+                   "Subgame","subgame.api.subscan.io",
+                   "Stafi","stafi.api.subscan.io",
+                   "Statemine","statemine.api.subscan.io",
+                   "Uniarts","uniarts.api.subscan.io",
+                   "Westend","westend.api.subscan.io")
+endpoints <- matrix(endpoint_list, ncol = 2, byrow = TRUE) %>%
+  as.data.table %>%
+  setnames(c("network_name","api_host"))
+
+# helper function
+getParams <- function(p) {
+
+  # i = 64
+  ti <- params[[i]] %>%
+    fromJSON(flatten=TRUE)
+
+  if (nrow(ti) == 3) {
+    amt_0 <- as.numeric(ti$value[[3]][1]) / 1e12
+    amt_1 <- NA
+    amt_2 <- NA
+    amt_3 <- NA
+  } else if (nrow(ti) == 4) {
+    amt_0 <- as.numeric(ti$value[[3]][1]) / 1e12
+    amt_1 <- as.numeric(ti$value[[4]][1]) / 1e12
+    amt_2 <- NA
+    amt_3 <- NA
+  } else if (nrow(ti) == 5) {
+    amt_0 <- as.numeric(ti$value[[3]][1]) / 1e12
+    amt_1 <- as.numeric(ti$value[[4]][1]) / 1e12
+    amt_2 <- as.numeric(ti$value[[5]][1]) / 1e12
+    amt_3 <- NA
+  } else if (nrow(ti) == 6) {
+    amt_0 <- as.numeric(ti$value[[3]][1]) / 1e12
+    amt_1 <- as.numeric(ti$value[[4]][1]) / 1e12
+    amt_2 <- as.numeric(ti$value[[5]][1]) / 1e12
+    amt_3 <- as.numeric(ti$value[[6]][1]) / 1e12
+  } else if (nrow(ti) > 6) {
+    stop("more than 4 pairs")
+  }
+
+  data.table(account = ti$value[[1]],
+             id_0 = ti$value[[2]]$Token[1],
+             id_1 = ti$value[[2]]$Token[2],
+             id_2 = ti$value[[2]]$Token[3],
+             id_3 = ti$value[[2]]$Token[4],
+             amt_0,
+             amt_1,
+             amt_2,
+             amt_3)
+
+}
+
+#' Get events from the Polkadot blockchain from the Subscan api
+#' https://docs.api.subscan.io
+#'
+#' @name get_subscan_events
+#' @title get_subscan_events
+#' @encoding UTF-8
+#' @concept Get events from the Polkadot blockchain from the Subscan api
+#' @param nobs integer how many transactions to pull
+#' @param network string indicating which Polkadot endpoint to use; defaults to 'Karura'.
+#' @param ss_module string indicating which module to pull; leave blank for all.
+#' @param ss_call string indicating which function call to pull; leave blank for all.
+#'
+#' @return data.table
+#'
+#' @examples
+#' tmp <- get_subscan_events(nobs = 111); dim(tmp$core_data)
+#' tmp <- get_subscan_events(nobs = 10, network = 'Karura', ss_module = 'dex', ss_call = 'Swap')
+#'
+#' @author Roger J. Bos, \email{roger.bos@@gmail.com}
+#' @export
+get_subscan_events <- function(nobs = 10, network = 'Karura', ss_module = '', ss_call = '') {
+
+  api_host <- endpoints[network_name == network, api_host]
+  api_call <- '/api/scan/events'
+  baseurl <- paste0('https://', api_host, api_call)
+
+  # each `page` pulls in 100 rows, so calculate how many pages you need to pull
+  last_page <- ceiling(max(1, (nobs /100)))
+
+  page_list <-list()
+  params_list <-list()
+  for (page in 1:last_page) {
+
+    body <- '{"row": ' %+% min(100, nobs) %+% ',"page": ' %+% page %+% ',"module": "' %+% ss_module %+% '","call": "' %+% ss_call %+% '"}'
+
+    r <- POST(baseurl, body = body,
+              add_headers(api_header, 'Content-Type=application/json'))
+    stop_for_status(r)
+    tmp <- content(r, as="text", encoding="UTF-8") %>%
+      fromJSON(flatten=TRUE)
+
+    core_data <- tmp$data$events %>%
+      as.data.table
+    params <- core_data$params
+    core_data <- core_data[, params := NULL]
+    core_data[, time := as.POSIXct(block_timestamp, origin = "1970-01-01", tz = 'UTC')]
+
+    print(nrow(core_data) %+% " rows for page " %+% page %+% " had " %+% tmp$message %+% " at " %+% Sys.time())
+
+    if (ss_module == 'dex' & ss_call == 'Swap') {
+      d <- list()
+      for (i in 1:length(params)) {
+        d[[i]] <- getParams(params[[i]])
+      }
+      out <- rbindlist(d)
+      all_data <- data.table(core_data, out)
+      page_list[[page]] <- all_data
+
+    } else {
+      page_list[[page]] <- core_data
+      params_list[[page]] <- params
+    }
+
+    # adjust for how many transaction left to pull
+    nobs <- nobs - nrow(core_data)
+  }
+
+  if (ss_module == 'dex' & ss_call == 'Swap') {
+    return(core_data = rbindlist(page_list))
+  } else {
+    return(list(core_data = rbindlist(page_list), params = params_list))
+  }
+
+}
+
+#' Get network metadata from the Subscan api
+#' https://docs.api.subscan.io
+#'
+#' @name get_subscan_metadata
+#' @title get_subscan_metadata
+#' @encoding UTF-8
+#' @concept Get network metadata from the Subscan api
+#' @param network string indicating which Polkadot endpoint to use; defaults to 'Karura'.
+#'
+#' @return list
+#'
+#' @examples
+#' get_subscan_metadata()
+#' get_subscan_metadata(network = 'Darwinia')
+#'
+#' @author Roger J. Bos, \email{roger.bos@@gmail.com}
+#' @export
+get_subscan_metadata <- function(network = 'Karura') {
+
+  api_host <- endpoints[network_name == network, api_host]
+  api_call <- '/api/scan/metadata'
+  baseurl <- paste0('https://', api_host, api_call)
+  r <- POST(baseurl, add_headers("Content-Type"="application/json"))
+  stop_for_status(r)
+
+  content(r, as="text", encoding="UTF-8") %>%
+    fromJSON(flatten=TRUE)
+}
+
+#' Get all events for a specific extrinsic id from the Subscan api
+#' https://docs.api.subscan.io
+#'
+#' @name get_subscan_extrinsic
+#' @title get_subscan_extrinsic
+#' @encoding UTF-8
+#' @concept Get all events for a specific extrinsic id from the Subscan api
+#' @param network string indicating which Polkadot endpoint to use; defaults to 'Karura'.
+#' @param extrinsic string extrinsic id to pull.
+#'
+#' @return list
+#'
+#' @examples
+#' get_subscan_extrinsic(extrinsic = '398539-2')
+#'
+#' @author Roger J. Bos, \email{roger.bos@@gmail.com}
+#' @export
+get_subscan_extrinsic <- function(network = 'Karura', extrinsic = '398539-2') {
+
+  api_call <- '/api/scan/extrinsic'
+  baseurl <- paste0('https://', api_host, api_call)
+  r <- POST(baseurl,
+            body = '{"extrinsic_index": "' %+% extrinsic %+% '"}')
+  stop_for_status(r)
+  tmp <- content(r, as="text", encoding="UTF-8") %>%
+    fromJSON(flatten=TRUE)
+  tmp$data
+
+}
