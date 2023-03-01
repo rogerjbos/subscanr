eb797e35e0da19ff65d5e0ce0d4358a4cd25aec9
rogerjbos <roger@parity.io>
2023-03-01 11:31:16 -0500

    added some defillama code

---
+++ R/defillama.R
@@ -0,0 +1,30 @@
+library(httr)
+library(jsonlite)
+
+baseurl <- 'https://api.llama.fi/protocols'
+r <- GET(url = baseurl)
+tmp <- content(r, as="text", encoding="UTF-8") %>%
+  fromJSON(flatten=TRUE) %>%
+  as.data.table
+rd <- melt(tmp, id=1:4, measure = grep("chainTvls", names(tmp)))
+rd <- rd[!is.na(value)]
+rd[, tag := gsub("chainTvls.","", variable)]
+utags <- unique(rd$tag)
+d <- strsplit(utags, "-") %>%
+  lapply(`[[`, 1) %>%
+  unlist
+defi <- sort(unique(d))
+
+
+
+for (id in defi) {
+  baseurl <- 'https://api.llama.fi/v2/historicalChainTvl/' %+% id
+  r <- GET(url = baseurl)
+  stopifnot(r$status_code == 200)
+  tmp <- content(r, as="text", encoding="UTF-8") %>%
+    fromJSON(flatten=TRUE) %>%
+    as.data.table
+  tmp[, date := as.POSIXct(date, origin = "1970-01-01")]
+  tmp[, chain := id]
+  
+}
